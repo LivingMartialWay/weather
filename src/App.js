7f75945caa5838a145bec78e219d645c63f39541
Daniel Bach <50657694+LivingMartialWay@users.noreply.github.com>
2020-06-21 13:54:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './style.css';
 import SearchBar from './containers/search_bar';
 import WeatherList from './containers/weather_list';
-import {logo} from './db.png';
+import logo from './db.png';
 
 export default class App extends Component {
   render() {
