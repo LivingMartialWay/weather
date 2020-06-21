73c04948d7c0097daff01db1cd7f25a678dbea40
Daniel Bach <50657694+LivingMartialWay@users.noreply.github.com>
2020-06-21 13:52:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './style.css';
 import SearchBar from './containers/search_bar';
 import WeatherList from './containers/weather_list';
+import {logo} from './db.png';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        {Logo /}
         <SearchBar />
         <WeatherList />
       </div>
